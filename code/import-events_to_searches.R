0d33c8ce4ebd637c3db44b42b0ace415d072053a
Daniel Marques <danmarquesvg@gmail.com>
2018-04-29 21:41:36 -0300

    Tratamento dos dados iniciais
    Foi compilado novos dados através do script "import_events_to_searchs.R" adicionando a coluna "session_time". Após isso, os dados foram filtrados, removendo alguns dados com erros.

--- code/import-events_to_searches.R
+++ code/import-events_to_searches.R
@@ -6,23 +6,26 @@ message("Lendo dados brutos de eventos")
 
 events = read_csv("https://github.com/wikimedia-research/Discovery-Hiring-Analyst-2016/raw/master/events_log.csv.gz")
 
-# events = events %>% slice(1:5e4) # Ãštil para testar cÃ³digo em dados pequenos. Comente na hora de processÃ¡-los para valer.
+#events = events %>% slice(1:5e4) # Útil para testar código em dados pequenos. Comente na hora de processá-los para valer.
 
 message("Transformando em dados por busca")
 
 events = events %>% 
     group_by(session_id) %>% 
     arrange(timestamp) %>% 
-    mutate(search_index = cumsum(action == "searchResultPage")) # contador para as buscas nessa sessÃ£o.
+    mutate(
+        search_index = cumsum(action == "searchResultPage"), # contador para as buscas nessa sessão.
+        session_time = difftime(ymd_hms(last(timestamp)), ymd_hms(first(timestamp)), units = "secs")
+    )
 
 searches = events %>% 
-    group_by(session_id, search_index) %>% 
+    group_by(session_id, search_index, session_time) %>% 
     arrange(timestamp) %>% 
     summarise(
         session_start_timestamp = first(timestamp),
         session_start_date = ymd_hms(first(timestamp)),
-        group = first(group), # eventos de uma mesma sessÃ£o sÃ£o de um mesmo grupo
-        results = max(n_results, na.rm = TRUE), # se nÃ£o houver busca, retorna -Inf
+        group = first(group), # eventos de uma mesma sessão são de um mesmo grupo
+        results = max(n_results, na.rm = TRUE), # se não houver busca, retorna -Inf
         num_clicks = sum(action == "visitPage"), 
         first_click = ifelse(num_clicks == 0, 
                              NA_integer_, 
@@ -31,7 +34,7 @@ searches = events %>%
     ) %>% 
     filter(search_index > 0) # Apenas search sessions
 
-out_file = here("data/search_data.csv")
+out_file = here("data/search_data2.csv")
 
 message("Salvando em ", out_file)
 
